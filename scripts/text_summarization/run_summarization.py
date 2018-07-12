c0afed7cc89d03e12f638bb1270638fe1ab7b4b0
Harden <group2ringsfield@gmail.com>
2018-07-13 05:48:40 +0800

    Train with the whole dataset

--- scripts/text_summarization/run_summarization.py
+++ scripts/text_summarization/run_summarization.py
@@ -1,9 +1,11 @@
-import sys
 import os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+sys.path.append("/search/hui/gluon-nlp-1")
 import io
-import collections
-import hashlib
-import time
+from time import time
 import argparse
 import random
 import logging
@@ -15,8 +17,7 @@ from gluonnlp.data import CorpusDataset
 from gluonnlp.data import count_tokens
 from mxnet.gluon.data import ArrayDataset, SimpleDataset
 from mxnet.gluon.data import DataLoader
-from transform import TrainValDataTransform
-from loss import SequenceLoss
+from transform_data import TrainValDataTransform
 import gluonnlp.data.batchify as btf
 from encoder_decoder import get_summ_encoder_decoder
 from summarization import SummarizationModel
@@ -24,20 +25,18 @@ from mxnet import gluon
 from mxnet.gluon.loss import SoftmaxCELoss
 
 
-import transform
-
 parser = argparse.ArgumentParser(description = 'Neural Abstractive Summarization')
 
 parser.add_argument('--dataset', type = str, default = '', help = 'Dataset to use.')
-parser.add_argument('--epochs', type = int, default = 100, help = 'upper epoch limit')
+parser.add_argument('--epochs', type = int, default = 5, help = 'upper epoch limit')
 parser.add_argument('--mode', type = str, default = 'train', help = 'Train/Validation/Test.')
 parser.add_argument('--experiment_name', type = str, default = 'experiment', help = 'experiment name')
-parser.add_argument('--hidden_dim', type = int, default = 256, help = 'dimension of RNN hidden states')
-parser.add_argument('--embedding_dim', type = int, default = 128, help = 'dimension of word embedding')
-parser.add_argument('--batch_size', type = int, default = 2, help = 'Batch Size')
+parser.add_argument('--hidden_dim', type = int, default = 64, help = 'dimension of RNN hidden states')
+parser.add_argument('--embedding_dim', type = int, default = 32, help = 'dimension of word embedding')
+parser.add_argument('--batch_size', type = int, default = 32, help = 'Batch Size')
 parser.add_argument('--test_batch_size', type = int, default = 16, help = 'Test Batch Size')
-parser.add_argument('--max_enc_steps', type = int, default = 5, help = 'max timesteps of encoder (max source text tokens)')
-parser.add_argument('--max_dec_steps', type = int, default = 6, help = 'max timesteps of decoder (max summary tokens)')
+parser.add_argument('--max_enc_steps', type = int, default = 200, help = 'max timesteps of encoder (max source text tokens)')
+parser.add_argument('--max_dec_steps', type = int, default = 50, help = 'max timesteps of decoder (max summary tokens)')
 parser.add_argument('--beam_size', type = int, default = 4, help = 'beam size for beam search decoding.')
 parser.add_argument('--min_dec_steps', type = int, default = 35, help = 'Minimum sequence length of generated summary. Applies only for beam search decoding mode')
 parser.add_argument('--vocab_size', type = int, default = 50000, help = 'Size of vocabulary.')
@@ -45,34 +44,34 @@ parser.add_argument('--optimizer', type = str, default = 'adam', help = 'Optimiz
 parser.add_argument('--lr', type = float, default = 0.15, help = 'Learning rate')
 parser.add_argument('--bucket_ratio', type = float, default = 0.0, help = 'bucket_ratio')
 parser.add_argument('--num_buckets', type = int, default = 1, help = 'bucket number')
-parser.add_argument('--gpu', type = int, default = None, help = 'id of the gpu to use. Set it to empty means to use cpu.')
-parser.add_argument('--clip', type = float, default = 3.0, help = 'gradient clipping')
+parser.add_argument('--gpu', type = int, default = 0, help = 'id of the gpu to use. Set it to empty means to use cpu.')
+parser.add_argument('--clip', type = float, default = 5.0, help = 'gradient clipping')
+parser.add_argument('--log_interval', type=int, default=100, metavar='N', help='report interval')
 
 args = parser.parse_args()
 
 # data_path = args.dataset
 
-data_path = "/Users/Admin/Documents/DL/gluon-nlp-1/scripts/text_summarization/finished_files"
+data_path = "data/finished_files"
 train_path = os.path.join(data_path, "train.txt")
 val_path = os.path.join(data_path, "val.txt")
 test_path = os.path.join(data_path, "test.txt")
 
 data_transform = TrainValDataTransform(max_enc_steps= args.max_enc_steps, max_dec_steps= args.max_dec_steps)
 
-train_data, train_data2idx, my_vocab = data_transform(dataset = train_path, makevocab = True)
-val_data, val_data2idx, _ = data_transform(dataset = val_path, vocab = my_vocab)
-test_data, test_data2idx, _ = data_transform(dataset = test_path, vocab = my_vocab)
+t0 = time()
+train_data_ori, train_data2idx, my_vocab = data_transform(dataset = train_path, makevocab = True)
+val_data_ori, val_data2idx, _ = data_transform(dataset = val_path, vocab = my_vocab)
+test_data_ori, test_data2idx, _ = data_transform(dataset = test_path, vocab = my_vocab)
+logging.info('Processing data spent {}'.format(time() - t0))
 
-print(type(train_data), train_data[0])
+data_train_lengths = [(len(ele[0]), len(ele[1])) for i, ele in enumerate(train_data_ori)]
+data_val_lengths = [(len(ele[0]), len(ele[1])) for i, ele in enumerate(val_data_ori)]
+data_test_lengths = [(len(ele[0]), len(ele[1])) for i, ele in enumerate(test_data_ori)]
 
-data_train_lengths = [(len(ele[0]), len(ele[1])) for i, ele in enumerate(train_data)]
-data_val_lengths = [(len(ele[0]), len(ele[1])) for i, ele in enumerate(val_data)]
-data_test_lengths = [(len(ele[0]), len(ele[1])) for i, ele in enumerate(test_data)]
-
-
-train_data = SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1])) for i, ele in enumerate(train_data2idx)])
-val_data = SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1]), i) for i, ele in enumerate(val_data2idx)])
-test_data = SimpleDataset([(ele[0], ele[1], len(ele[0]), len(ele[1]), i) for i, ele in enumerate(test_data2idx)])
+train_data = SimpleDataset([(ele[0], ele[1], ele[2], len(ele[0]), len(ele[1]), len(ele[2])) for i, ele in enumerate(train_data2idx)])
+val_data = SimpleDataset([(ele[0], ele[1], ele[2], len(ele[0]), len(ele[1]), len(ele[2]), i) for i, ele in enumerate(val_data2idx)])
+test_data = SimpleDataset([(ele[0], ele[1], ele[2], len(ele[0]), len(ele[1]), len(ele[2]), i) for i, ele in enumerate(test_data2idx)])
 
 if args.gpu is None:
     ctx = mx.cpu()
@@ -84,18 +83,10 @@ encoder, decoder = get_summ_encoder_decoder(hidden_size = args.hidden_dim)
 
 model = SummarizationModel(vocab = my_vocab, encoder = encoder, decoder = decoder, hidden_dim = args.hidden_dim, embed_size = args.embedding_dim, prefix = 'summary_')
 
-loss_function = SequenceLoss(valid_length= args.max_dec_steps, batch_size = args.batch_size, vocab_size = args.vocab_size)
-loss_function.initialize(init = mx.init.Uniform(0.1), ctx =ctx)
-loss_function.hybridize()
-print "#50: Create model"
-model = SummarizationModel(vocab = my_vocab, encoder = encoder, decoder = decoder, hidden_dim = args.hidden_dim, embed_size = args.embedding_dim, batch_size = args.batch_size, prefix = 'summary_')
-
-# loss_function = SequenceLoss(valid_length= abs_valid_length, vocab_size = args.vocab_size)
-print "#54: Create loss_function"
 loss_function = SoftmaxCELoss()
 loss_function.initialize(init = mx.init.Uniform(0.1), ctx = ctx)
 loss_function.hybridize()
-print "#56"
+# print "#56"
 model.initialize(init = mx.init.Uniform(0.1), ctx = ctx)
 model.hybridize()
 
@@ -133,20 +124,21 @@ def evaluate(data_loader):
     return avg_loss, real_translation_out
 
 def run_train():
-    print "#57: trainer"
+    # print "#57: trainer"
     trainer = gluon.Trainer(model.collect_params(), args.optimizer, {'learning_rate':args.lr})
-    print "#58: batchify"
+    # print "#58: batchify"
 
-    train_batchify_fn = btf.Tuple(btf.Pad(), btf.Pad(), btf.Stack(), btf.Stack())
+    t0 = time()
+    train_batchify_fn = btf.Tuple(btf.Pad(), btf.Pad(), btf.Pad(), btf.Stack(), btf.Stack(), btf.Stack(),)
 
-    test_batchify_fn = btf.Tuple(btf.Pad(), btf.Pad(), btf.Stack(), btf.Stack(), btf.Stack())
+    test_batchify_fn = btf.Tuple(btf.Pad(), btf.Pad(), btf.Pad(), btf.Stack(), btf.Stack(), btf.Stack(), btf.Stack())
 
     train_batch_sampler = FixedBucketSampler(lengths = data_train_lengths,
                                                 batch_size = args.batch_size,
                                                 num_buckets = args.num_buckets,
                                                 ratio = args.bucket_ratio,
-                                                shuffle = True)
-    logging.info('Train Batch Sampler:\n{}'.format(train_batch_sampler.stats()))
+                                                shuffle = False)
+    print('Train Batch Sampler:\n{}'.format(train_batch_sampler.stats()))
     train_data_loader = DataLoader(train_data,
                                     batch_sampler = train_batch_sampler,
                                     batchify_fn=train_batchify_fn,
@@ -158,7 +150,7 @@ def run_train():
                                                 ratio = args.bucket_ratio,
                                                 shuffle = False)
 
-    val_data_loader = DataLoader(  val_data,
+    val_data_loader = DataLoader(val_data,
                                     batch_sampler = val_batch_sampler,
                                     batchify_fn=test_batchify_fn,
                                     num_workers=8)
@@ -174,26 +166,24 @@ def run_train():
                                     batchify_fn=test_batchify_fn,
                                     num_workers=8)
 
-    # # #for # DEBUG:
-    # art_seq = mx.nd.random.uniform(shape=(args.batch_size,7))
-    # abs_seq = mx.nd.random.uniform(shape=(args.batch_size,7))
-    # art_seq = art_seq.as_in_context(ctx)
-    # abs_seq = abs_seq.as_in_context(ctx)
-    # art_valid_length = mx.ndarray.ones(shape=(args.batch_size,))*7
-    # abs_valid_length = mx.ndarray.ones(shape=(args.batch_size,))*6
-    # # print art_valid_length
-    # art_valid_length = art_valid_length.as_in_context(ctx)
-    # abs_valid_length = abs_valid_length.as_in_context(ctx)
+    print('Batchifying spent {}'.format(time() - t0))
 
-    print "trianning loop"
+    # print "trianning loop"
     for epoch_id in range(args.epochs):
-        print "epoch_id: ", epoch_id
-        for batch_id, (art_seq, abs_seq, art_valid_length, abs_valid_length) in enumerate(train_data_loader):
-            print "batch_id: ", batch_id
+        log_avg_loss = 0
+        log_avg_gnorm = 0
+        log_wc = 0
+        log_start_time = time()
+        print("epoch_id: {}".format(epoch_id))
+        for batch_id, (art_seq, abs_seq, abs_target, art_valid_length, abs_valid_length, target_valid_length) in enumerate(train_data_loader):
+            print("batch_id: ", epoch_id, batch_id)
+            ts = time()
             art_seq = art_seq.as_in_context(ctx)
             abs_seq = abs_seq.as_in_context(ctx)
+            abs_target = abs_target.as_in_context(ctx)
             art_valid_length = art_valid_length.as_in_context(ctx)
             abs_valid_length = abs_valid_length.as_in_context(ctx)
+            target_valid_length = target_valid_length.as_in_context(ctx)
             with mx.autograd.record():
                 #out should be our prediction with shape
                 '''
@@ -202,19 +192,32 @@ def run_train():
                 decoder_outputs = model(art_seq, abs_seq[:, :-1], art_valid_length, abs_valid_length - 1)
                 # targets = mx.ndarray.one_hot(abs_seq, args.vocab_size)
                 decoder_outputs = mx.ndarray.stack(*decoder_outputs, axis = 1)
-                loss = loss_function(decoder_outputs, abs_seq[:, :-1]).mean()
-                loss = loss * abs_seq.shape[1] / (abs_valid_length-1).mean()
+                loss = loss_function(decoder_outputs, abs_target[:, :-1]).mean()
+                loss = loss * abs_target.shape[1] / (target_valid_length-1).mean()
                 loss.backward()
 
             grads = [p.grad(ctx) for p in model.collect_params().values()]
             gnorm = gluon.utils.clip_global_norm(grads, args.clip)
             step_loss = loss.asscalar()
-            print "step_loss: ", step_loss
+            print ("{}: {}: spent {}s and step_loss: {}".format(epoch_id, batch_id, time() - ts, step_loss))
             art_wc = art_valid_length.sum().asscalar()
             abs_wc = (abs_valid_length - 1).sum().asscalar()
             log_avg_loss += step_loss
             log_avg_gnorm += gnorm
             log_wc += art_wc + abs_wc
+            if (batch_id + 1) % args.log_interval == 0:
+                wps = log_wc / (time() - log_start_time)
+                print('[Epoch {} Batch {}/{}] loss={:.4f}, ppl={:.4f}, gnorm={:.4f}, '
+                             'throughput={:.2f}K wps, wc={:.2f}K'
+                             .format(epoch_id, batch_id + 1, len(train_data_loader),
+                                     log_avg_loss / args.log_interval,
+                                     np.exp(log_avg_loss / args.log_interval),
+                                     log_avg_gnorm / args.log_interval,
+                                     wps / 1000, log_wc / 1000))
+                log_start_time = time()
+                log_avg_loss = 0
+                log_avg_gnorm = 0
+                log_wc = 0
 
 
 
---
+++ scripts/text_summarization/transform_data.py
@@ -0,0 +1,126 @@
+import os
+import time
+import gluonnlp
+from mxnet.gluon.data import ArrayDataset
+import re
+import codecs
+import pickle
+import numpy as np
+embed_type = 'glove'
+embd_source = 'glove.6B.50d.txt'
+
+class TrainValDataTransform(object):
+    def __init__(self, max_enc_steps, max_dec_steps, embedding_type = 'glove', source = 'glove.6B.50d.txt'):
+        self._embedding_type = embedding_type
+        self._source = source
+        self._max_enc_step = max_enc_steps
+        self._max_dec_step = max_dec_steps
+
+    def __call__(self, dataset, makevocab = False, vocab = None):
+        if makevocab:
+            return self.trans(dataset, makevocab, vocab)
+        else:
+            return self.trans(dataset, vocab = vocab)
+
+    def get_embedding(self):
+        return gluonnlp.embedding.create(self._embedding_type, self._source)
+
+    def build_vocab(self, vocab_file):
+        # Builidding vocabulary
+        # my_vocab: embedding, idx_to_token, reserved_token, token_to_idx, unknown_token, padding_token, bos_token, eos_token
+        print("Building vocabulary...")
+        with codecs.open(vocab_file, "rb") as fvocab:
+            my_vocab = pickle.loads(fvocab)
+
+        return my_vocab
+
+    def trans(self, dataset, makevocab = False, vocab = None):
+
+        vocabfile = "data/finished_files_2/my_vocab"
+        vocab_exit = os.path.isfile(vocabfile)
+
+        if makevocab:
+            if vocab_exit:
+                with open(vocabfile, "rb") as fvocab:
+                    print(vocabfile)
+                    my_vocab = pickle.load(fvocab, encoding="utf-8")
+            else:
+                raise Exception("please build your vocab first")
+        else:
+            my_vocab = vocab
+
+        if my_vocab is None:
+            raise Exception("my_vocab cannot be None")
+
+        with codecs.open(dataset, 'r', 'utf-8') as f:
+            lines = f.readlines()
+
+        start_id = my_vocab.token_to_idx[my_vocab.bos_token]
+        stop_id = my_vocab.token_to_idx[my_vocab.eos_token]
+        art_list = []
+        abs_list = []
+        dec_inp_list = []
+        dec_target_list = []
+        art2idx = []
+        # abs2idx = []
+
+        cnt = 0
+        t0 = time.time()
+        for line in lines:
+            line = line.strip()
+            if not re.search('\t', line):
+                cnt += 1
+                print("{} should have split symbol".format(cnt))
+                try:
+                    print(line)
+                except:
+                    print ("ascii")
+                continue
+            line = line.split('\t')
+            art = line[0].split()
+            abs = line[1].split()
+
+
+            art_list.append(art)
+            abs_list.append(abs)
+            # art2idx.append(my_vocab(art))
+            # abs2idx.append(my_vocab(abs))
+            art_line = my_vocab[art]
+            abs_line = my_vocab[abs]
+            if len(art_line) > self._max_enc_step:
+                art_line = art_line[:self._max_enc_step]
+
+            if cnt < 2:
+                print(art_line)
+                print(type(art_line[0]))
+
+            art2idx.append(art_line)
+
+            dec_inp, dec_target = self.get_dec_inp_target_seqs(abs_line, self._max_dec_step,start_id, stop_id)
+
+            dec_inp_list.append(dec_inp)
+            dec_target_list.append(dec_target)
+
+            if cnt%5000 == 0:
+                print("{} have processed".format(cnt))
+
+            cnt += 1
+
+        print("Transform data spent {}".format(time.time() - t0))
+
+        data = ArrayDataset(art_list,abs_list)
+        # print data
+        data_idx = ArrayDataset(np.array(art2idx), np.array(dec_inp_list), np.array(dec_target_list))
+        # print data_idx
+        return data, data_idx, my_vocab
+
+    def get_dec_inp_target_seqs(self, sequence, max_len, start_id, stop_id):
+        inp = [start_id] + sequence[:]
+        target = sequence[:]
+        if len(inp) > max_len:
+            inp = inp[:max_len]
+            target = target[:max_len]
+        else:
+            target.append(stop_id)
+        assert len(inp) == len(target)
+        return inp, target
